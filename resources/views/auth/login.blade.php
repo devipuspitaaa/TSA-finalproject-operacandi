715bb851c61fbd2c6d6d97437fa83251c07ff2fc
devipuspitaaa <puspitasaridevi228@gmail.com>
2022-10-19 09:11:58 +0700

    perbaikan login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,18 +23,19 @@
                     </div>
                     <br>
                     <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
+                        <form method="POST" action="{{ route('login') }}" id="form-login">
 
                             @csrf
 
                             <div class="row mb-3">
+                                <input type="hidden" name="level" />
                                 <label for="username" class="col-md-4 col-form-label text-md-end">{{ __('Username')
                                     }}</label>
 
                                 <div class="col-md-6">
                                     <input id="username" type="username"
                                         class="form-control @error('username') is-invalid @enderror" name="username"
-                                        value="{{ old('username') }}" required autocomplete="username" autofocus>
+                                        value="{{ old('username') }}" autocomplete="username">
 
                                     @error('username')
                                     <span class="invalid-feedback" role="alert">
@@ -51,7 +52,7 @@
                                 <div class="col-md-6">
                                     <input id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="current-password">
+                                        autocomplete="current-password">
 
                                     @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -101,4 +102,119 @@
             </div>
         </div>
     </div>
+
+    
+
+    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+
+    <div class="toast bg-danger text-white fade" id="myToast" style="position: absolute; top: 10px; right: 10px;" >
+        <div class="toast-body">
+            Harap pilih level login yang tersedia
+        </div>
+    </div>
+
+
+    <script>
+        $(function() {
+
+            // inisialisasi input
+            var level = $('input[name="level"]');
+            
+            var btnAdmin = $('#btn_admin');
+            var btnPengawas = $('#btn_pengawas');
+            var btnLurah = $('#btn_lurah');
+
+            // btn admin dipencet
+            $('#btn_admin').click(function() {
+
+                level.val("admin");
+
+                if ( $(this).hasClass("btn-info") ){
+
+                    $(this).removeClass("btn-info");
+                    $(this).addClass("btn-warning");
+                }
+
+                if ( btnPengawas.hasClass("btn-warning") ) {
+
+                    btnPengawas.removeClass("btn-warning");
+                    btnPengawas.addClass("btn-info");
+                }
+
+                if ( btnLurah.hasClass("btn-warning") ) {
+
+                    btnLurah.removeClass("btn-warning");
+                    btnLurah.addClass("btn-info");
+                }
+            });
+
+            $('#btn_pengawas').click(function() {
+
+                level.val("pengawas");
+
+
+
+                if ( $(this).hasClass("btn-info") ){
+
+                    $(this).removeClass("btn-info");
+                    $(this).addClass("btn-warning");
+                }
+
+                if ( btnAdmin.hasClass("btn-warning") ) {
+
+                        btnAdmin.removeClass("btn-warning");
+                        btnAdmin.addClass("btn-info");
+                }
+
+                if ( btnLurah.hasClass("btn-warning") ) {
+
+                        btnLurah.removeClass("btn-warning");
+                        btnLurah.addClass("btn-info");
+                }
+            });
+
+            $('#btn_lurah').click(function() {
+
+                level.val("lurah");
+
+
+                if ( $(this).hasClass("btn-info") ){
+
+                    $(this).removeClass("btn-info");
+                    $(this).addClass("btn-warning");
+                }
+
+
+                if ( btnAdmin.hasClass("btn-warning") ) {
+
+                        btnAdmin.removeClass("btn-warning");
+                        btnAdmin.addClass("btn-info");
+                }
+
+                if ( btnPengawas.hasClass("btn-warning") ) {
+
+                        btnPengawas.removeClass("btn-warning");
+                        btnPengawas.addClass("btn-info");
+                }
+            });
+
+
+
+            $('#form-login').submit(function( e ){
+
+                if ( level.val().length > 0 ) {
+
+                    $(this).submit();
+                } else {
+
+                    $("#myToast").toast("show");
+                }
+
+                e.preventDefault();
+            });
+
+        });
+    </script>
+
+
     @endsection
